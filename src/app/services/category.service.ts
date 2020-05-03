0561bf2dce74e82abf6b7ea5a13900c3aebdd63c
vddastranger <vladdaking@gmail.com>
2020-05-03 17:12:30 +0300

    Update services

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs/internal/Observable';
 import {CategoryModel} from '../models/category.model';
 import {url} from '../../environments/environment';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,10 @@ export class CategoryService {
 
   getUserCategories(): Observable<CategoryModel[]> {
     return this.http.get<CategoryModel[]>(`${url.API}/getUserCategories?id=${localStorage.getItem('userId')}`);
+  }
+
+  createCategory(category: CategoryModel): Observable<HttpResponse> {
+    return this.http.post<HttpResponse>(`${url.API}/createCategory`, category, {observe: 'response'})
+      .pipe(map((response: HttpResponse) => response.status));
   }
 }
