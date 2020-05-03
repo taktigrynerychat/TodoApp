5d8af048c45e1cb961577443b84852f1d9671829
vddastranger <vladdaking@gmail.com>
2020-05-03 21:56:40 +0300

    stupid forms i hate it

--- src/app/modules/dashboard/components/task-comments/task-comments.component.ts
+++ src/app/modules/dashboard/components/task-comments/task-comments.component.ts
@@ -4,12 +4,13 @@ import {CommentModel} from '../../../../models/comment.model';
 import {Observable} from 'rxjs/internal/Observable';
 import {CommentService} from '../../../../services/comment.service';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {of} from "rxjs/internal/observable/of";
 
 @Component({
   selector: 'app-task-comments',
   templateUrl: './task-comments.component.html',
   styleUrls: ['./task-comments.component.less'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TaskCommentsComponent implements OnInit, OnChanges {
 
@@ -25,10 +26,6 @@ export class TaskCommentsComponent implements OnInit, OnChanges {
               private fb: FormBuilder) {
   }
 
-  saveComment() {
-    console.log(this.commentForm.value);
-  }
-
   ngOnInit(): void {
     this.userLogin = JSON.parse(localStorage.getItem('user')).login;
   }
@@ -36,11 +33,20 @@ export class TaskCommentsComponent implements OnInit, OnChanges {
   ngOnChanges(): void {
     if (this.taskId) {
       this.comments = this.commentService.getTaskComments(this.taskId);
-      this.commentForm = this.fb.group({
-        task_id: this.taskId,
-        text: null
-      });
+      this.createCommentForm();
     }
   }
 
+  saveComment() {
+    console.log(this.commentForm.value);
+    this.createCommentForm();
+    this.comments = of<CommentModel[]>([{ ...this.commentForm.value, id: 2}]);
+  }
+
+  createCommentForm() {
+    this.commentForm = this.fb.group({
+      task_id: this.taskId,
+      text: null
+    });
+  }
 }
