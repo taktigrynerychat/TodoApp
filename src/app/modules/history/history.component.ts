c9bb24dd38a080e802d0188063c67bbeb39511d2
vddastranger <vladdaking@gmail.com>
2020-07-18 14:43:51 +0300

    Unsubscription logic to prevent data leaks

--- src/app/modules/history/history.component.ts
+++ src/app/modules/history/history.component.ts
@@ -1,20 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {TaskService} from '../../services/task.service';
-import {Observable} from "rxjs/internal/Observable";
-import {TaskModel} from "../../models/task.model";
-import {map} from "rxjs/operators";
-import {TasksService} from "../../state/tasks/tasks.service";
-import {TasksQuery} from "../../state/tasks/tasks.query";
-import {CategoriesService} from "../../state/categories/categories.service";
+import {Observable} from 'rxjs/internal/Observable';
+import {TaskModel} from '../../models/task.model';
+import {map, takeUntil} from 'rxjs/operators';
+import {TasksService} from '../../state/tasks/tasks.service';
+import {TasksQuery} from '../../state/tasks/tasks.query';
+import {CategoriesService} from '../../state/categories/categories.service';
+import {Subject} from 'rxjs/internal/Subject';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.less']
 })
-export class HistoryComponent implements OnInit {
+export class HistoryComponent implements OnInit, OnDestroy {
 
   tasks: Observable<TaskModel[]>;
+  private unsub = new Subject();
 
   constructor(private tasksService: TasksService,
               private tasksQuery: TasksQuery,
@@ -23,8 +25,12 @@ export class HistoryComponent implements OnInit {
 
   ngOnInit(): void {
     this.tasks = this.tasksQuery.doneTasks$;
-    this.categoriesService.getUserCategories().subscribe();
-    this.tasksService.getAllTasks().subscribe();
+    this.categoriesService.getUserCategories().pipe(takeUntil(this.unsub)).subscribe();
+    this.tasksService.getAllTasks().pipe(takeUntil(this.unsub)).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.unsub.unsubscribe();
   }
 
 }
