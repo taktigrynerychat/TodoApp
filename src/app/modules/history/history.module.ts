b3712a5c47e805cc04d2d5fe9e5839aa8114818c
Vlad <vladdaking@gmail.com>
2021-04-22 18:50:10 +0300

    change history

--- src/app/modules/history/history.module.ts
+++ src/app/modules/history/history.module.ts
@@ -1,5 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import {HistoryComponent} from './history.component';
 import {PageLayoutModule} from '../page-layout/page-layout.module';
 
@@ -8,8 +10,10 @@ import {PageLayoutModule} from '../page-layout/page-layout.module';
   declarations: [HistoryComponent],
   imports: [
     CommonModule,
-    PageLayoutModule
-  ]
+    PageLayoutModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+  ],
 })
 export class HistoryModule {
 }
